759e6cd7211401f859ccd6f70c6c096219574674
DanielLeonD <daniel.leond@udea.edu.co>
2023-08-17 22:44:02 -0500

    Fungi egg shadow size fix

--- src/main/java/net/dain/hongozmod/item/ModItems.java
+++ src/main/java/net/dain/hongozmod/item/ModItems.java
@@ -23,23 +23,27 @@ public class ModItems {
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> ZHONGO_SPAWN_EGG = ITEMS.register("zhongo_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.ZHONGO, 0x0d0c8, 0xd6c3aa,
+            () -> new ForgeSpawnEggItem(ModEntityTypes.ZHONGO, 0x00d0c8, 0xd6c3aa,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> HORDEN_SPAWN_EGG = ITEMS.register("horden_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.HORDEN, 0x0d0c8, 0xd6c3aa,
+            () -> new ForgeSpawnEggItem(ModEntityTypes.HORDEN, 0x00d0c8, 0xd6c3aa,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> CROAKTAR_SPAWN_EGG = ITEMS.register("croaktar_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.CROAKTAR, 0x0d0c8, 0xd6c3aa,
+            () -> new ForgeSpawnEggItem(ModEntityTypes.CROAKTAR, 0x00d0c8, 0xd6c3aa,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> HONZIADE_SPAWN_EGG = ITEMS.register("honziade_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.HONZIADE, 0x0d0c8, 0xd6c3aa,
+            () -> new ForgeSpawnEggItem(ModEntityTypes.HONZIADE, 0x00d0c8, 0xd6c3aa,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> HUNTER_SPAWN_EGG = ITEMS.register("hunter_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.HUNTER, 0x0d0c8, 0xd6c3aa,
+            () -> new ForgeSpawnEggItem(ModEntityTypes.HUNTER, 0x00d0c8, 0xd6c3aa,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> HUNTER_TEST_SPAWN_EGG = ITEMS.register("hunter_test_spawn_egg",
+            () -> new ForgeSpawnEggItem(ModEntityTypes.HUNTER, 0xffa8a8, 0xd6d6d6,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> HONZIADE_QUEEN_SPAWN_EGG = ITEMS.register("honziade_queen_spawn_egg",
